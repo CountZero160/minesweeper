8f7a613681ab65cfc86bee61c3e8353b991acd99
Patrick <pschilber@gmail.com>
2023-09-11 17:01:40 -0400

    possibly final updates

--- src/Tile.js
+++ src/Tile.js
@@ -3,6 +3,7 @@ import React from "react";
 export default function Tile({ details, updateFlag, revealTile }) {
   return (
     <div
+      className="tile"
       onContextMenu={(e) => updateFlag(e, details.x, details.y)}
       onClick={() => revealTile(details.x, details.y)}
       style={{
@@ -19,3 +20,45 @@ export default function Tile({ details, updateFlag, revealTile }) {
     </div>
   );
 }
+
+const chexPattern = (x, y) => {
+  if (x % 2 === 0 && y % 2 === 0) {
+    return "#aad751";
+  } else if (x % 2 === 0 && y % 2 !== 0) {
+    return "#a2d249";
+  } else if (x % 2 !== 0 && y % 2 === 0) {
+    return "#a2d249";
+  } else {
+    return "#aad751";
+  }
+};
+
+const bombChexPattern = (x, y) => {
+  if (x % 2 === 0 && y % 2 === 0) {
+    return "#e5c29f";
+  } else if (x % 2 === 0 && y % 2 !== 0) {
+    return "#d7b899";
+  } else if (x % 2 !== 0 && y % 2 === 0) {
+    return "#d7b899";
+  } else {
+    return "#e5c29f";
+  }
+};
+
+const numColorCode = (num) => {
+  if (num === 1) {
+    return "#1976d2";
+  } else if (num === 2) {
+    return "#388d3c";
+  } else if (num === 3) {
+    return "#d33030";
+  } else if (num === 4) {
+    return "#7c21a2";
+  } else if (num === 5) {
+    return "#1976d2";
+  } else if (num === 6) {
+    return "#1976d2";
+  } else {
+    return "white";
+  }
+};
